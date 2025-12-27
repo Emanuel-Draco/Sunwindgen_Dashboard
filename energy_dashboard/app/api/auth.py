49dcc9db1fb1b95e63335ba6a8660655c2ac2017
Emanuel-Draco <117759593+Emanuel-Draco@users.noreply.github.com>
2025-12-27 19:37:17 +0530

    Update auth.py

--- energy_dashboard/app/api/auth.py
+++ energy_dashboard/app/api/auth.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from pydantic import BaseModel
-from fastapi import response
 
 from auth.users import get_user
 from auth.security import verify_password, create_access_token
@@ -14,12 +13,13 @@ class LoginRequest(BaseModel):
 @router.post("/login")
 def login(data: LoginRequest, response: Response):
     user = get_user(data.username)
+
     if not user or not verify_password(data.password, user["password_hash"]):
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = create_access_token(user["username"])
 
-    # cookie HTTP-only (ważne!)
+    # HTTP-only cookie
     response.set_cookie(
         key="session",
         value=token,
